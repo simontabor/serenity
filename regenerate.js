320dcf86dfdbd55d780591d39ba624479ee85653
Simon Tabor <me@simontabor.com>
2012-08-31 01:24:32 +0100

    this has been hugely improved. generates properly, runs better on command line when globally install and a load more

--- regenerate.js
+++ regenerate.js
@@ -1,8 +1,9 @@
-module.exports = function(root,files,config) {
-	var ejs = require('ejs'),
-	fs = require('fs'),
-	rm = require('rimraf'),
-	cli = require('cli');
+var ejs = require('ejs'),
+fs = require('fs'),
+rm = require('rimraf'),
+cli = require('cli');
+
+module.exports = function(files,config) {
 
   var count = 0;
   var made = function(total) {
@@ -11,47 +12,56 @@ module.exports = function(root,files,config) {
     }
 
   };
-  var render = function(file,path) {
-   fs.readFile(file,'ascii',function(err,file) {
+  var renderejs = function(file,path) {
+    console.log(file,path);
+   fs.readFile(file,'ascii',function(err,filec) {
+    console.log(err);
     var opts;
     try {
-      opts = JSON.parse(file.split('______')[0]);
+      opts = JSON.parse(filec.split('______')[0]);
     }catch(e) {
+      cli.error('No config specified for '+file);
       opts = {};
     }
-    console.log(opts);
-    path[path.length -1] = path[path.length-1].replace('.ejs','.html');
-    opts.path = path.join('/');
-    var html = ejs.render(file.split('______')[1] || file,{
+    path = path.replace('.ejs','.html');
+    opts.path = path.split('./_site')[1];
+    var html = ejs.render(filec.split('______')[1] || filec,{
       page: opts,
-      site: config || {}
+      site: config
     });
-    fs.writeFile(root+'/_site/'+path.join('/'),html,function(err) {
+    fs.writeFile(path,html,function(err) {
     });
   });
  };
 
- rm(root+'/_site',function() {
-  cli.debug('Deleted '+root+'/_site');
-  fs.mkdir(root+'/_site',function() {
-   cli.debug('Created '+root+'/_site');
+ rm('./_site',function() {
+  cli.debug('Deleted ./_site');
+  fs.mkdir('./_site',function() {
+   cli.debug('Created ./_site');
    for (var i = 0; i< files.length; i++) {
-    var path = files[i].split(root+'/')[1].split('/');
-    var filename = path[path.length -1];
-    if (path[0].indexOf('_') !== 0 && filename != 'config.json') {
+    var path = files[i].substr(1,files[i].length);
+    var patharr = files[i].substr(2,files[i].length).split('/');
+    var filename = patharr[patharr.length -1];
+
+    var ignored = false;
+    for (var j = 0;j<config.ignore.length;j++) {
+      if ((filename == config.ignore[j]) || (path == config.ignore[j])) ignored = true;
+    }
+    if (path.indexOf('_') !== 1 && !ignored) {
 
           // make directories
-          for (var j=0;j<path.length-1;j++) {
+          for (var j=0;j<patharr.length-1;j++) {
             var dir = '';
-            for (var p = 0; p<j;p++) {
-              dir += '/'+path[p];
+            for (var p = 0; p<=j;p++) {
+              dir+=patharr[p]+'/';
             }
-            fs.mkdir(root+'/_site'+dir+'/'+path[j], made(files.length));
+            console.log('mkdir '+dir);
+            fs.mkdir('./_site/'+dir, made(files.length));
           }
           var ext = filename.split('.');
           ext = ext[ext.length-1];
           if (ext == 'ejs') {
-            render(files[i],path);
+            renderejs(files[i],'./_site'+path);
           }
         }else{
           made(files.length);
--- templr.js
+++ templr.js
@@ -3,59 +3,67 @@
 var cli = require('cli'),
 fs = require('fs'),
 config,
-childProcess = require('child_process'),
-exec = childProcess.exec,
-regen = require('./regenerate.js'),
-files;
+regen = require('./regenerate.js'), // the file regeneration script
+root = process.cwd(), // where templr has been executed from.
+files; // array of files within the directory
 
+cli.parse({
+  port:  ['p', 'Listen on this port - overrides any config values', 'number', 4000]
+});
 
 cli.main(function (args,options) {
-  // ensure no trailing slash
-  if (args[0].charAt(args[0].length-1) == "/") args[0] = args[0].substr(0,args[0].length-1);
- try {
-  config = require(args[0]+'/config.json');
-}catch(e) {
-  cli.error('No config file found at '+args[0]);
-}
-var walk = function(dir, done) {
-  var results = [];
-  fs.readdir(dir, function(err, list) {
-    if (err) return done(err);
-    var pending = list.length;
-    if (!pending) return done(null, results);
-    list.forEach(function(file) {
-      file = dir +'/'+ file;
-      fs.stat(file, function(err, stat) {
-        var path = file.split('/');
-        console.log(path,path[path.length-1]);
-        if (path[path.length-1] == '_site') {
-          if (!--pending) done(null, results);
-        }else{
-          if (stat && stat.isDirectory()) {
-            walk(file, function(err, res) {
-              results = results.concat(res);
-              if (!--pending) done(null, results);
-            });
-          } else {
-            results.push(file);
+console.log('Templr watching: ' + root);
+  try {
+    config = require('./templr.json');
+  }catch(e) {
+    config = {
+      ignore: ['templr.json','README.md']
+    };
+    cli.error('No config file found at ./templr.json');
+  }
+  var walk = function(dir, done) {
+    var results = [];
+    fs.readdir(dir, function(err, list) {
+      if (err) return done(err);
+      var pending = list.length;
+      if (!pending) return done(null, results);
+      list.forEach(function(file) {
+        file = dir + '/'+file;
+        fs.stat(file, function(err, stat) {
+
+          var path = file.split('/');
+
+          if (path[path.length-1] == '_site') {
             if (!--pending) done(null, results);
+          }else{
+            if (stat && stat.isDirectory()) {
+              walk(file, function(err, res) {
+                results = results.concat(res);
+                if (!--pending) done(null, results);
+              });
+            } else {
+              results.push(file);
+              if (!--pending) done(null, results);
+            }
           }
-        }
+        });
       });
     });
+  };
+  var fileChanged = function (curr, prev) {
+    if (curr.mtime > prev.mtime) {
+      cli.debug('File changed, regenerating.');
+      regen(files,config);
+    }
+  };
+
+  // lets start walking through our root!
+  walk('.',function(err,list) {
+    files = list;
+    regen(files,config);
+    for (var i = 0; i < list.length; i++) {
+      fs.watchFile(list[i],{interval:100},fileChanged);
+    }
   });
-};
-var fileChanged = function (curr, prev) {
-  if (curr.mtime > prev.mtime) {
-  cli.debug('File changed, regenerating.');
-  regen(args[0],files,config);
-}
-};
-walk(args[0],function(err,list) {
-  files = list;
-  for (var i = 0; i < list.length; i++) {
-    fs.watchFile(list[i],{interval:100},fileChanged);
-  }
-});
 });
 
