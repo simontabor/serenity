3aea5a3c3259c474531f2758cb6180a0c8126d6b
Simon Tabor <me@simontabor.com>
2014-01-27 11:47:37 +0000

    v0.1.2 autoprefixer browser options

--- defaults.js
+++ defaults.js
@@ -4,6 +4,7 @@ var config = {
     "ignoreCommonPatterns": true,
     "ignorePaths": [process.cwd()+'/_site']
 	},
+  autoprefixer: [], // array of browser options for autoprefixer
   ignore: ['/_site','/.git'],
   extensions: ['*'], // files with these extensions will be parsed and output
   asset_host: ''
