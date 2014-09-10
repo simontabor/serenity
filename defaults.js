3034ca4c11f60ab82bb8fdf8b779fe5b0b179185
Simon Tabor <me@simontabor.com>
2014-09-10 16:19:24 +0100

    add markdown to default config

--- defaults.js
+++ defaults.js
@@ -1,9 +1,16 @@
-var config = {
-  autoprefixer: [], // array of browser options for autoprefixer
-  ignore: [ '/_site', '/.git', '/node_modules' ], // folder patterns to ignore
-  extensions: ['*'], // files with these extensions will be parsed and output
-  asset_host: '', // define an asset host (such as a CDN) for all compiled assets
-  workers: require('os').cpus().length // number of generation workers to spawn and use
+module.exports = {
+  // array of browser options for autoprefixer
+  autoprefixer: [],
+  // folder patterns to ignore
+  ignore: [ '/_site', '/.git', '/node_modules' ],
+  // files with these extensions will be parsed and output
+  extensions: ['*'],
+  // define an asset host (such as a CDN) for all compiled assets
+  asset_host: '',
+  // number of generation workers to spawn and use
+  workers: require('os').cpus().length,
+  // whether to parse <markdown> or <md> elements as markdown.
+  // set to the `marked` configuration object.
+  // if markdown.highlight is truthy, we'll use highlight.js on code blocks
+  markdown: false
 };
-
-module.exports = config;
